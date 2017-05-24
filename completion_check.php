d50dccc9b6586068a2b972e24c05a8cfc91302c3
Kyle Maharlika <kyle.maharlika@gmail.com>
2017-05-24 15:12:33 +0000

    Added changes to utilize timecompleted, timestarted, timelaunched

--- completion_check.php
+++ completion_check.php
@@ -24,7 +24,7 @@
 
 require_once('header.php');
 require_once($CFG->dirroot."/mod/tincanlaunch/TinCanPHP/autoload.php");
-global $USER, $DB;
+global $USER, $DB, $COURSE;
 $completion = new completion_info($course);
 
 $result = $DB->get_records('config_plugins', array('plugin' => 'tincanlaunch'));
@@ -62,8 +62,9 @@ $response = $lrs->queryStatements(array(
     'related_activities' => true
 ));
 $activityData =  processStatements($response->content->getStatements());
+//$data = explode('captivate-activity',$activityData['97bfc34e-e349-4e7a-98a5-96fb0d4edd72']['activity']); //working to add condition for captivate-activity only
 
-//to get all data from tincan for launched activity
+//to get all data, from tincan for launched activity
 if ($completion->is_enabled($cm) && $tincanlaunch->tincanverbid) {
     $completion->update_state($cm, COMPLETION_COMPLETE);
 
@@ -80,26 +81,40 @@ if ($completion->is_enabled($cm) && $tincanlaunch->tincanverbid) {
     $date = date("Y-m-d H:i:s");
     $cur_date = strtotime($date);
     $activityId = $DB->get_record('course_modules_completion', array('coursemoduleid'=> $context->instanceid,'userid'=> $USER->id));
-    
+    $latest_session_data = current($activityData);
+    $latest_access_time = strtotime($latest_session_data['completed']);
+
     // to check if there is a record in completion_module_table and user has launched/updating first attempt
-    if(!empty($activityId) && count($activityData) == 1){ 
+    
+ //   if(!empty($activityId) && count($activityData) == 1){ 
+    if(!empty($activityId)){ 
         $time = '';
         $activityResponse = '';
+        $activityData = array_reverse($activityData); // $activityData have latest record from lrs, reversed data to get data of first attemted response
         foreach($activityData as $eachAct){
-           $eachAct['responses'] = array_reverse($eachAct['responses']); //reversed array to get the first attempted data in case user went through slides multiple time
-           foreach($eachAct['responses'] as $each_response){
-                if(strpos(strtolower($each_response['text']),'feedback') == true){ // check if user has experienced feedback slide
-                    $getCompletedTime = explode('on' ,$each_response['text']);
-                    $time = end($getCompletedTime); //get the last value of array i.e.time
-                    $convertTime = strtotime($time);
+            $check_ativity_type = explode('captivate-activity',$eachAct['activity']); //added condition for captivate-activity only
+            if(!empty($eachAct['responses']) && sizeOf($check_ativity_type) > 1){
+                $eachAct['responses'] = array_reverse($eachAct['responses']); //reversed array to get the first attempted data in case user went through slides multiple time
+                foreach($eachAct['responses'] as $each_response){
+                     if(strpos(strtolower($each_response['text']),'feedback') == true){ // check if user has experienced feedback slide
+                         $getCompletedTime = explode('on' ,$each_response['text']);
+                         $time = end($getCompletedTime); //get the last value of array i.e.time
+                         $convertTime = strtotime($time);
 
-                    $data = array('id'=> $activityId->id,
-                                  'timecompleted'=> $convertTime,
-                                  'timemodified'=>$convertTime
+                         $data = array('id'=> $activityId->id,
+                                       'timecompleted'=> $convertTime,
+                                       'timemodified'=>$convertTime
+                                 );
+                         $DB->update_record('course_modules_completion', $data);
+                         exit;
+                     }
+                 }
+                 //to update the last access time
+                $dataLastAccess = array('id'=> $activityId->id,
+                               'lastaccess'=> $latest_access_time
                             );
-                    $DB->update_record('course_modules_completion', $data);
-                    exit;
-                }
+                $DB->update_record('course_modules_completion', $dataLastAccess);
+                exit;
             }
         }
 
